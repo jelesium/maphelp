653799ce3ab8fdd2812413344f0a3b26c12642de
jelesium <jelesium@gmail.com>
2019-12-14 20:27:32 -0500

    some edits

--- src/components/WebMap.js
+++ src/components/WebMap.js
@@ -1,24 +1,70 @@
-import React, { useEffect, useRef } from "react";
+import React from 'react';
+import ArcGISMap from 'esri/Map';
+import MapView from 'esri/views/MapView';
 
-export function WebMap() {
-  const elementRef = useRef();
+export class WebMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xCoord : -118
+    }
+    this.mapRef = React.createRef();
+  }
 
-  useEffect(_ => {
-    let cleanup;
-    // lazy load the module that loads the JSAPI
-    // and initialize it
-    import("../data/app").then(
-      app => cleanup = app.initialize(elementRef.current)
+  componentDidMount() {
+    // create map
+    const map = new ArcGISMap({
+      basemap: 'topo-vector'
+    });
+    // load the map view at the ref's DOM node
+    this.view = new MapView({
+      container: this.mapRef.current,
+      map: map,
+      center: [this.state.xCoord, 34],
+      zoom: 8
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
+  }
+
+  handleNewAddress(){
+
+    this.setState({
+      xCoord: (this.state.xCoord - 1)
+    }, console.log('working'))
+    const map = new ArcGISMap({
+      basemap: 'topo-vector'
+    });
+    this.view = new MapView({
+      container: this.mapRef.current,
+      map: map,
+      center: [this.state.xCoord, 34],
+      zoom: 8
+    });
+    console.log(this.state.xCoord, 'XCOORD')
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            {this.state.xCoord? this.state.xCoord: null}
+          </li>
+          <li onMouseOver = {() => this.handleNewAddress()}>
+            Los Angeles
+          </li>
+          <li>
+            Florida
+          </li>
+        </ul>
+      <div className="webmap" ref={this.mapRef} />
+      </div>
     );
-    return () => cleanup && cleanup();
-  }, []);
-
-  // assign elementRef to the ref of our component
-  return (
-    <div className="viewDiv" ref={elementRef}>
-    </div>
-  );
+  }
 }
-
-
-
